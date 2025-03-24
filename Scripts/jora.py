22c5b0f4161bd3a982a057380049cc18f3fefeed
srdobolo <joaomiguellime@gmail.com>
2025-03-24 22:00:52 +0000

    Update jora.py

--- Scripts/jora.py
+++ Scripts/jora.py
@@ -8,6 +8,18 @@ def jora():
     # Base URL for the find-jobs section
     base_url = 'https://recruityard.com/find-jobs-all/'
 
+    # Employment type mapping
+    employment_type_map = {
+        'FULL_TIME': 'Full time',
+        'PART_TIME': 'Part time',
+        'CONTRACTOR': 'Contract',
+        'TEMPORARY': 'Casual/Temporary',
+        'INTERN': 'Internship',
+        'VOLUNTEER': 'Volunteer',
+        'PER_DIEM': 'Working Holiday',
+        'OTHER': 'Freelance'
+    }
+
     # Load the main jobs page to find all job links
     response = requests.get(base_url)
     html_content = response.content
@@ -20,7 +32,7 @@ def jora():
     rss_feed = f'''<?xml version="1.0" encoding="UTF-8"?>
 <source>
     <publisher>Recruityard</publisher>
-    <lastBuildDate>{datetime.datetime.now}</lastBuildDate>'''
+    <lastBuildDate>{datetime.datetime.now()}</lastBuildDate>'''
 
     # Iterate over each job link, fetch its content, and extract the JSON
     for job_link in job_links:
@@ -35,6 +47,25 @@ def jora():
             json_content = html.unescape(script_tag.string)
             try:
                 data = json.loads(json_content)
+
+                # Extract salary value
+                salary_value = data.get('baseSalary', {}).get('value', {}).get('value', 'undisclosed')
+                min_salary = 'undisclosed'
+                max_salary = 'undisclosed'
+
+                # Process salary if it's a string and contains numbers
+                if isinstance(salary_value, (str, int, float)) and str(salary_value).replace('.', '').isdigit():
+                    salary_str = str(salary_value)
+                    min_salary = salary_str[:4] if len(salary_str) >= 4 else salary_str
+                    max_salary = salary_str[-5:] if len(salary_str) > 4 else salary_str[-4:]
+
+                # Map employment type
+                raw_employment_type = data.get('employmentType', 'undisclosed')
+                # If employmentType is a list, take the first value; otherwise, use as-is
+                if isinstance(raw_employment_type, list):
+                    raw_employment_type = raw_employment_type[0] if raw_employment_type else 'undisclosed'
+                employment_type = employment_type_map.get(raw_employment_type, 'Freelance') if raw_employment_type != 'undisclosed' else 'undisclosed'
+
                 rss_feed += f'''
             <job>
               <title><![CDATA[{data.get('title', 'undisclosed')}]]></title>
@@ -49,11 +80,11 @@ def jora():
               <description><![CDATA[{data.get('description', 'undisclosed')}]]></description>
               <salary>
                 <type>Monthly</type>
-                <min> XXX </min>                                                                                        #arranjasr isto
-                <max> XXX </max>                                                                                        #arranjar isto    
+                <min><![CDATA[{min_salary}]]></min>
+                <max><![CDATA[{max_salary}]]></max>
                 <currency><![CDATA[{data.get('baseSalary', {}).get('currency', 'undisclosed')}]]></currency>
               </salary>
-              <jobtype><![CDATA[{data.get('employmentType', 'undisclosed')}]]></jobtype>                                #arranjar isto
+              <jobtype><![CDATA[{employment_type}]]></jobtype>
               <url><![CDATA[{job_url}]]></url>
             </job>'''
             except json.JSONDecodeError:
